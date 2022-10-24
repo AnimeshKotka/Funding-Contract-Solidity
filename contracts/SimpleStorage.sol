54f09ec7b5921869c1bded268246aa7f15710a29
AnimeshKotka <animeshk@capitalnumbers.com>
2022-10-24 12:08:02 +0530

    added js and setting up blockchain locally

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,26 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.8;
+pragma solidity ^0.8.7;
 
-contract SimpleStorage{
-
+contract SimpleStorage {
     uint256 favNumber;
 
-    struct People{
+    struct People {
         uint256 number;
-        string name; 
+        string name;
     }
 
     People[] public people;
 
     mapping(string => uint256) public personMapping;
 
-
-
     // Stack, memory, storage, call data, logs, code -> need this to store the values
     // calldata -> it is temp var which can not be modified
     // memory -> it is temp var which can be modified
     // storage -> it will stay to scope after it ended like global var (parmanent, can't be modified)
-    // Here memory we need because sol can't add memory to array, struct and maps 
+    // Here memory we need because sol can't add memory to array, struct and maps
     function addPeople(string memory _name, uint256 _number) public {
         people.push(People(_number, _name));
         personMapping[_name] = _number;
@@ -30,8 +27,7 @@ contract SimpleStorage{
         favNumber = _favNumber;
     }
 
-    function retrive() public view returns(uint256){
+    function retrive() public view returns (uint256) {
         return favNumber;
     }
-
 }
